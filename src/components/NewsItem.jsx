0424b1d1c0016e1be7dc18a86cf9ab25f318064d
Tanay998 <65861229+Tanay998@users.noreply.github.com>
2025-06-09 22:54:08 +0530

    add some changes in index.html, add file sampleoutput.json, add some changes in News.jsx and NewsItem.jsx

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <>
-        This is a News Item 2
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Readmore</a>
+          </div>
+        </div>
       </>
     )
   }
