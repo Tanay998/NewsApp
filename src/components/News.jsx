36221386d8073ceb8d85e16dce76093f6a42520d
Tanay998 <65861229+Tanay998@users.noreply.github.com>
2025-06-10 22:55:14 +0530

    add some changes in files

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,71 +2,46 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 
 export default class News extends Component {
-  articles =[
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "England vs West Indies LIVE: Second T20, Bristol - cricket score, radio, highlights & updates",
-            "description": "England face West Indies in the second T20 in Bristol - follow live scores, radio commentary, video highlights and updates.",
-            "url": "http://www.bbc.co.uk/sport/cricket/live/c87pqj0vy2et",
-            "urlToImage": "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/2d40/live/4e831bd0-446b-11f0-b6e6-4ddb91039da1.jpg",
-            "publishedAt": "2025-06-08T13:37:16.2204385Z",
-            "content": "England XI: Smith, Duckett, Buttler (wk), Brook (capt), Banton, Bethell, Jacks, Dawson, Carse, Rashid, L Wood.\r\nWest Indies XI: Lewis, Charles, Hope (capt &amp; wk), Chase, Powell, Rutherford, Shephe… [+34 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
-  constructor(){
+  constructor() {
     super();
     console.log("Hello I am constructor from News Component");
     this.state = {
-      articles: this.articles,
-      loading:false
+      articles: [],
+      loading: false
     }
-    
+
+  }
+  async componentDidMount() {
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=&category=business&apiKey=d6484692fff743309c2420e629dff1b6";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles
+    });
+
   }
   render() {
+    console.log("render");
+
     return (
       <>
         <div className="container my-3">
           <h2>News Ninja - Top News</h2>
-          <div className="row">
-            <div className="col-md-4">
-              <NewsItem title = "myTitle" description = "desc" imageUrl = "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/2d40/live/4e831bd0-446b-11f0-b6e6-4ddb91039da1.jpg" newsUrl = "Todo" />
-            </div>
-            <div className="col-md-4">
-              <NewsItem title = "myTitle" description = "desc" />
-            </div>
-            <div className="col-md-4">
-              <NewsItem title = "myTitle" description = "desc" />
-            </div>
+          <div className="row row-cols-1 row-cols-md-3 g-4 mt-4">
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={element.description ? element.description.slice(0, 85) : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              )
+            })}
           </div>
         </div>
       </>
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,18 +2,36 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
+    
+    // Set default image if imageUrl is not provided
+    const defaultImage = "https://image.cnbcfm.com/api/v1/image/108133398-1744901282932-gettyimages-2210727739-anotherday18620636_mw2vfms2.jpeg?v=1749418062&w=1920&h=1080";
+    
     return (
-      <>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail/" className="btn btn-sm btn-primary">Readmore</a>
-          </div>
+      <div className="card h-100">
+        <img 
+          src={imageUrl || defaultImage} 
+          className="card-img-top" 
+          alt={title || "News image"}
+          style={{ height: "200px", objectFit: "cover" }}
+          onError={(e) => {
+            e.target.onerror = null;  // Prevent infinite loop
+            e.target.src = defaultImage;
+          }}
+        />
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text flex-grow-1">{description}.....</p>
+          <a 
+            href={newsUrl} 
+            target="_blank" 
+            rel="noreferrer"
+            className="btn btn-sm btn-primary mt-2 align-self-start"
+          >
+            Read more
+          </a>
         </div>
-      </>
+      </div>
     )
   }
 }
